10a0e6e8d7ab16cf08b8340181064a96a9ee537a
Gojaehyeon <gojaehyeonstudio@gmail.com>
2025-06-10 18:38:45 +0900

    Initial Commit

---
+++ batteryGoTests/batteryGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  batteryGoTests.swift
+//  batteryGoTests
+//
+//  Created by 고재현 on 6/10/25.
+//
+
+import Testing
+@testable import batteryGo
+
+struct batteryGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
